f33f64c1c0a08bc272d9a1e2a1d973cd3b6e512b
youhaolin <cador.ai@aliyun.com>
2018-08-11 14:45:01 +0800

    update

--- data_proc.py
+++ data_proc.py
@@ -62,7 +62,7 @@ def trim_md(md_file):
                     check_dict[kv[0].strip(' ')] = json.loads(kv[1].strip(' ').strip('\n'))
             if log:
                 if len(row_content) > 0:
-                    content.append(row)
+                    content.append(row.strip('\n'))
             if not start and row_content == "+++":
                 start = True
             elif start and row_content == "+++":
